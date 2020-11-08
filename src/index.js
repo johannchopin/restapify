5c754cd9a8cc023ee3c1bad227ca648ffc665e5d
johannchopin <johannchopin@pm.me>
2020-11-08 23:51:26 +0100

    ✨ Handle number casting

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const express = require('express')
 const app = express()
 
 const HTTP_VERBS = ['get', 'put', 'delete', 'create']
+const NUMBER_CAST_INDICATOR = '(number)'
 
 const BASE = resolve(__dirname, '../test/api/')
  
@@ -14,15 +15,30 @@ const replaceAll = (str, find, replace) => {
  return str.split(find).join(replace)
 }
 
+const getNumbersToCast = (string) => {
+  const re = /\"\(number\)\[([^,}]+)\]\"/gi
+  const match = string.match(re)
+
+  return match !== null ? match : []
+}
+
 const getVarsInPath = (path) => {
-   const re = /\[([^}]+)\]/gi
-   const match = path.match(re)
-   return match !== null ? match : []
+  const vars = []
+  const explodedPath = path.split('/')
+
+  explodedPath.forEach(pathElement => {
+    const isVar = pathElement.startsWith('[') && pathElement.endsWith(']')
+    if (isVar) {
+      vars.push(pathElement.slice(1, -1))
+    }
+  })
+
+  return vars
 }
 
 const getNormalizedApiRoute = (path, params) => {
   params.forEach(param => {
-    path = replaceAll(path, param, `:${param.slice(1, -1)}`)
+    path = replaceAll(path, `[${param}]`, `:${param}`)
   })
 
   return path
@@ -33,10 +49,10 @@ const getRoute = (filePath, filename) => {
   const params = getVarsInPath(route)
   const apiRoute = getNormalizedApiRoute(route, params).replace(filename, '')
   const fileVariable = filename.split('.')[0]
-  const varInFilename = getVarsInPath(filename)[0]
+  const varInFilename = getVarsInPath(fileVariable)[0]
 
   if (varInFilename) {
-    return apiRoute.split('/').slice(0, -1).join('/') + '/:' + varInFilename.slice(1, -1)
+    return apiRoute.split('/').slice(0, -1).join('/') + '/:' + varInFilename
   }
 
   if (fileVariable === '*') {
@@ -48,18 +64,25 @@ const getRoute = (filePath, filename) => {
 
 const configFile = (filePath, filename) => {
   const httpVerbInFilename = filename.split('.')[-2]
-  const fileContent = readFileSync(filePath, 'utf8')
+  let fileContent = readFileSync(filePath, 'utf8')
   const route = filePath.replace(BASE, '')
+  const numberParamsToCast = getNumbersToCast(fileContent)
   const params = getVarsInPath(route)
   const apiRoute = getRoute(filePath, filename)
+
+  numberParamsToCast.forEach(numberParamToCast => {
+    console.log(numberParamToCast, numberParamToCast.slice(`"${NUMBER_CAST_INDICATOR}`.length, -1))
+    fileContent = replaceAll(fileContent, numberParamToCast, numberParamToCast.slice(`"${NUMBER_CAST_INDICATOR}`.length, -1))
+  })
 
   const responseCallback = (req, res) => {
     let stringifyJson = fileContent
 
     params.forEach(variable => {
-      stringifyJson = replaceAll(stringifyJson, variable, req.params[variable.slice(1, -1)])
+      stringifyJson = replaceAll(stringifyJson, variable, req.params[variable])
     })
 
+    console.log(stringifyJson)
     res.send(JSON.parse(stringifyJson))
   }
 
