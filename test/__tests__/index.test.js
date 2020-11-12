566e3951bfd0d33a226afc444331839d77483323
johannchopin <johannchopin@pm.me>
2020-11-12 15:30:15 +0100

    ✅ Init testing

---
+++ test/__tests__/index.test.js
@@ -0,0 +1,23 @@
+import * as path from 'path'
+import Restapify from '../../src/index'
+
+const restapifyParams = {
+  rootDir: path.resolve(__dirname, '../api'),
+  port: 6767,
+  apiPrefix: '/api'
+}
+
+const apiRoot = `http://localhost:${restapifyParams.port}${restapifyParams.apiPrefix}`
+
+const RestapifyInstance = new Restapify(restapifyParams)
+RestapifyInstance.run()
+
+describe('Restapify', () => {
+  describe('Running server', () => {
+    it('should respond to a simple GET', async () => {
+      let response = await fetch(`${apiRoot}/users`);
+      let data = await response.json()
+      console.log(data)
+    });
+  });
+});
---
+++ test/__tests__/utils/server.test.js
@@ -0,0 +1,31 @@
+import { getVarsInPath } from '../../../src/utils/server'
+
+describe('getVarsInPath', () => {
+  it('find no vars in path', () => {
+    const path = '/posts'
+    const expectedFoundVars = []
+
+    expect(getVarsInPath(path)).toStrictEqual(expectedFoundVars)
+  })
+
+  it('find 1 vars in path', () => {
+    const path = '/users/[userid]'
+    const expectedFoundVars = ['userid']
+
+    expect(getVarsInPath(path)).toStrictEqual(expectedFoundVars)
+  })
+
+  it('find 2 vars in path', () => {
+    const path = '/posts/[postid]/comments/[commentid]'
+    const expectedFoundVars = ['postid', 'commentid']
+
+    expect(getVarsInPath(path)).toStrictEqual(expectedFoundVars)
+  })
+
+  it('find 2 vars in path with json extension', () => {
+    const path = '/posts/[postid]/comments/[commentid].json'
+    const expectedFoundVars = ['postid', 'commentid']
+
+    expect(getVarsInPath(path)).toStrictEqual(expectedFoundVars)
+  })
+})
