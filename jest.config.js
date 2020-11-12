aaa14b6eaca583f873af3dcd769aa5fa246aec1f
johannchopin <johannchopin@pm.me>
2020-11-12 15:29:46 +0100

    🔧 jest

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  rootDir: "test",
+  transform: {
+    '^.+\\.js?$': 'babel-jest',
+    '^.+\\.ts?$': 'ts-jest'
+  },
+  moduleFileExtensions: ['js', 'ts'],
+  bail: false,
+  verbose: true
+}
