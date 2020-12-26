55e67318768f0f03fe2df4e314f5dcc96971aec6
johannchopin <johannchopin@pm.me>
2020-12-26 19:39:23 +0100

    ✨ Reorder HTTP method order

--- src/CONST.ts
+++ src/CONST.ts
@@ -1,6 +1,6 @@
 import { HttpVerb } from './types'
 
 export const CURRENT_LOCATION_ROUTE_SELECTOR = '*'
-export const HTTP_VERBS: HttpVerb[] = ['GET', 'POST', 'DELETE', 'PUT', 'PATCH']
+export const HTTP_VERBS: HttpVerb[] = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
 export const INTERNAL_API_BASEURL = '/restapify/api'
 export const NUMBER_CAST_INDICATOR = 'n:'
