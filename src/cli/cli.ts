377efea2022261510891c31c89fc25a98e8b1b6c
johannchopin <johannchopin@pm.me>
2020-12-28 00:04:56 +0100

    ♻️ Move start server logic in separated file

--- src/cli/cli.ts
+++ src/cli/cli.ts
@@ -1,59 +1,8 @@
-import * as path from 'path'
-import * as chalk from 'chalk'
 import { program } from 'commander'
 
 import * as packageJson from '../../package.json'
 
-import Restapify, { RestapifyParams } from '../Restapify'
-import { getInstanceOverviewOutput, getMethodOutput, onRestapifyInstanceError } from './utils'
-
-const startRestapifyServer = (options: RestapifyParams): void => {
-  const {
-    rootDir,
-    port,
-    baseUrl,
-    openDashboard
-  } = options
-
-  const RestapifyInstance = new Restapify({
-    rootDir: path.resolve(rootDir),
-    port: port,
-    baseUrl: baseUrl,
-    openDashboard: openDashboard
-  })
-  RestapifyInstance.on('server:start', () => {
-    console.log(`\n🏗 Try to serve on port ${RestapifyInstance.port}`)
-  })
-
-  RestapifyInstance.onError(({ error }) => {
-    onRestapifyInstanceError(error, {
-      rootDir: RestapifyInstance.rootDir,
-      apiBaseUrl: RestapifyInstance.apiBaseUrl,
-      port: RestapifyInstance.port
-    })
-  })
-
-  RestapifyInstance.on('start', () => {
-    const servedRoutes = RestapifyInstance.getServedRoutes()
-
-    servedRoutes.forEach(servedRoute => {
-      let methodOutput = getMethodOutput(servedRoute.method)
-
-      console.log(`${methodOutput} ${servedRoute.route}`)
-    })
-
-    console.log(getInstanceOverviewOutput(
-      RestapifyInstance.port,
-      RestapifyInstance.apiBaseUrl
-    ))
-  })
-
-  RestapifyInstance.on('server:restart', () => {
-    console.log(chalk.green('✅ API updated!'))
-  })
-
-  RestapifyInstance.run()
-}
+import { startRestapifyServer } from './startRestapifyServer'
 
 export const cli = (cliArgs: string[]): void => {
   program
