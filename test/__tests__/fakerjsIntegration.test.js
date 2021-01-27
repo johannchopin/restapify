1704d84a9d205a83485d429da88aab8302f949e2
johannchopin <johannchopin@pm.me>
2021-01-27 20:36:37 +0100

    ✅ It should replace faker syntax by faker's data

--- test/__tests__/fakerjsIntegration.test.js
+++ test/__tests__/fakerjsIntegration.test.js
@@ -1,4 +1,9 @@
-import { getFakerVarsInContent } from '../../src/getRoute'
+import * as path from 'path'
+
+import {getRoute, getFakerVarsInContent } from '../../src/getRoute'
+
+// D A T A
+import getPostsById from '../api/posts/[postid]/*.json'
 
 describe('Faker\'s integration', () => {
   it('should find faker\' syntax in content', () => {
@@ -6,5 +11,18 @@ describe('Faker\'s integration', () => {
     const expectedResult = ['lorem:sentences', 'name:findName']
 
     expect(getFakerVarsInContent(content)).toStrictEqual(expectedResult)
+  })
+
+  it('should replace faker syntax by faker\'s data', () => {
+    const EMAIL_MATCHER = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
+    const route = getRoute(
+      path.resolve(__dirname, '../api/posts/[postid]/*.json'),
+      path.resolve(__dirname, '../api'),
+      JSON.stringify(getPostsById)
+    )
+
+    const bodyResponse = JSON.parse(route.getBody({postid: 'my-post'}))
+
+    expect(bodyResponse.email).toMatch(EMAIL_MATCHER)
   })
 })
