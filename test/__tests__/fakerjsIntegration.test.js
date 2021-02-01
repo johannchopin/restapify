1917d0fffe86a4fd5adefdd3ef1e9ed8a1d523ce
johannchopin <johannchopin@pm.me>
2021-02-01 14:37:17 +0100

    ✅ Test using mock on faker

--- test/__tests__/fakerjsIntegration.test.js
+++ test/__tests__/fakerjsIntegration.test.js
@@ -1,10 +1,23 @@
 import * as path from 'path'
+import * as faker from 'faker';
 
 import { getRoute } from '../../src/getRoute'
 import { getFakerVarsInContent, getContentWithReplacedFakerVars } from '../../src/fakerHelpers'
 
 // D A T A
 import getPostsById from '../api/posts/[postid]/*.json'
+
+jest.mock('faker', () => ({
+  lorem: {
+    text: jest.fn().mockImplementation(() => 'fake' ),
+  },
+  internet: {
+    email: jest.fn().mockImplementation(() => 'fake@email.com' ),
+  },
+  time: {
+    recent:jest.fn().mockImplementation(() => 123 ),
+  }
+}))
 
 describe('Faker\'s integration', () => {
   it('should find faker\'s syntax in content', () => {
@@ -15,16 +28,17 @@ describe('Faker\'s integration', () => {
   })
 
   it('should replace faker syntax by faker\'s data', () => {
-    const EMAIL_MATCHER = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
     const route = getRoute(
       path.resolve(__dirname, '../api/posts/[postid]/*.json'),
       path.resolve(__dirname, '../api'),
       JSON.stringify(getPostsById)
     )
+  
+    route.getBody({postid: 'my-post'})
 
-    const bodyResponse = JSON.parse(route.getBody({postid: 'my-post'}))
-
-    expect(bodyResponse.email).toMatch(EMAIL_MATCHER)
+    expect(faker.lorem.text).toHaveBeenCalledTimes(1)
+    expect(faker.internet.email).toHaveBeenCalledTimes(1)
+    expect(faker.time.recent).toHaveBeenCalledTimes(1)
   })
 
   it('should replace faker syntax with number cast syntax', () => {
