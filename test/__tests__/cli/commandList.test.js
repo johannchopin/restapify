6fa12a6060088bc72a7e88d0bf74e3f58d5ddf45
johannchopin <johannchopin@pm.me>
2021-02-09 15:25:36 +0100

    ✅ CLI list

---
+++ test/__tests__/cli/commandList.test.js
@@ -0,0 +1,34 @@
+import * as path from 'path'
+
+import { cli } from '../../../src/cli/cli'
+
+const consoleLogSpy = jest.spyOn(global.console, 'log')
+
+describe('Test `list` command', () => {
+  describe('List routes', () => {
+    const pathToApiFolder = path.resolve(__dirname, '../../api')
+    const args = `yarn restapify list ${pathToApiFolder}`
+    let logOutput
+
+    beforeEach(() => {
+      cli(args.split(' '))
+      logOutput = consoleLogSpy.mock.calls[0][0]
+    })
+
+    it('should contain correct routes', () => {
+      expect(logOutput).toEqual(
+        expect.stringContaining('/api/animals')
+      )
+      expect(logOutput).toEqual(
+        expect.stringContaining('/api/posts/[postid]/comments/[commentid]')
+      )
+      expect(logOutput).toEqual(
+        expect.stringContaining('/api/users/[userid]/friends')
+      )
+    })
+
+    it('should output correct number of routes', () => {
+      expect(logOutput.split('\n').length).toBe(22)
+    })
+  })
+})
