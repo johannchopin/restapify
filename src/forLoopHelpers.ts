2ad83fef752cc42c4dbdd084a0959d7b89ee3424
johannchopin <johannchopin@pm.me>
2021-02-25 22:46:48 +0100

    ✨ Handle faker syntax in for-loop

--- src/forLoopHelpers.ts
+++ src/forLoopHelpers.ts
@@ -6,6 +6,7 @@ import {
   FOR_LOOP_SYNTAX_PREFIX,
   FOR_LOOP_SYNTAX_SUFFIX
 } from './const'
+import { getContentWithReplacedFakerVars } from './fakerHelpers'
 
 const ELMT_BETWEEN_PARENTHESES_MATCHER = /\(([^)]+)\)/g
 
@@ -70,13 +71,19 @@ export const getArrayFromRangeString = (stringifiedRange: string): number[] => {
   return []
 }
 
-export const getSequenceArray = (sequence: string): (number | string)[] => {
+export const getSequenceArrayAsArray = (sequence: string): (number | string | boolean)[] => {
+  sequence = getContentWithReplacedFakerVars(sequence)
+  sequence = replaceAll(sequence, '\'', '"')
+
+  return JSON.parse(sequence)
+}
+
+export const getSequenceArray = (sequence: string): (number | string | boolean)[] => {
   const isSequenceAnArray = sequence.startsWith('[') && sequence.endsWith(']')
   const isSequenceRange = sequence.startsWith('range(') && sequence.endsWith(')')
 
   if (isSequenceAnArray) {
-    sequence = replaceAll(sequence, '\'', '"')
-    return JSON.parse(sequence)
+    return getSequenceArrayAsArray(sequence)
   } if (isSequenceRange) {
     return getArrayFromRangeString(sequence)
   }
