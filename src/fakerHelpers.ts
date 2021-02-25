7d70aad656346ed5453c10e3f4d4441eca7d2b55
johannchopin <johannchopin@pm.me>
2021-02-25 22:18:12 +0100

    ✨ Handle boolean for faker

--- src/fakerHelpers.ts
+++ src/fakerHelpers.ts
@@ -5,7 +5,7 @@ import {
   FAKER_SYNTAX_SUFIX
 } from './const'
 import { FakerSyntaxData } from './types'
-import { getVarCastSyntax, replaceAll } from './utils'
+import { replaceAllCastedVar } from './utils'
 
 export const getFakerVarsInContent = (content: string): string[] => {
   return Array.from(content.matchAll(FAKER_SYNTAX_MATCHER), m => m[1])
@@ -35,25 +35,39 @@ export const areFakerVarsSyntaxValidInContent = (content: string): true | FakerS
   return invalidFakerSyntaxData !== null ? invalidFakerSyntaxData : true
 }
 
+export const getFakerDataSyntax = (data: FakerSyntaxData): string => {
+  const { namespace, method } = data
+  return `${FAKER_SYNTAX_PREFIX}${namespace}:${method}${FAKER_SYNTAX_SUFIX}`
+}
+
+export const replaceAllCastedFakerVar = (
+  content: string,
+  variable: FakerSyntaxData,
+  value: string
+): string => {
+  const fakerSyntax = getFakerDataSyntax(variable)
+  return replaceAllCastedVar(content, fakerSyntax.substring(1, fakerSyntax.length - 1), value)
+}
+
 export const getContentWithReplacedFakerVars = (content: string): string => {
   const fakerVars = getFakerVarsInContent(content)
 
   fakerVars.forEach((fakerVar) => {
     const fakerVarSyntax = `${FAKER_SYNTAX_PREFIX}${fakerVar}${FAKER_SYNTAX_SUFIX}`
-    const [fakerNamespace, fakerMethod] = fakerVar.split(':')
+    const [namespace, method] = fakerVar.split(':')
     // @ts-ignore
-    const fakedData = faker[fakerNamespace][fakerMethod]()
-    const sanitizedFakedData = JSON.stringify(fakedData)
-    const fakedDataValue = sanitizedFakedData.substring(1, sanitizedFakedData.length - 1)
+    const fakedData = faker[namespace][method]()
+    let sanitizedFakedData = JSON.stringify(fakedData)
 
-    content = replaceAll(
-      content,
-      getVarCastSyntax(fakerVarSyntax.substring(1, fakerVarSyntax.length - 1), 'number'),
-      fakedDataValue
-    )
+    const isFakedDataString = sanitizedFakedData.startsWith('"') && sanitizedFakedData.endsWith('"')
+    if (isFakedDataString) {
+      sanitizedFakedData = sanitizedFakedData.substring(1, sanitizedFakedData.length - 1)
+    }
+
+    content = replaceAllCastedFakerVar(content, { namespace, method }, sanitizedFakedData)
     content = content.replace(
       fakerVarSyntax,
-      fakedDataValue
+      sanitizedFakedData
     )
   })
 
