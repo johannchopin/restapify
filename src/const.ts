a8877002df2db77166f4ae9e252a134c74ae1bae
johannchopin <johannchopin@pm.me>
2021-06-23 16:01:34 +0200

    ♻️ Consistent regex matcher

--- src/const.ts
+++ src/const.ts
@@ -24,6 +24,6 @@ export const FAKER_SYNTAX_PREFIX = '[#faker:'
 export const FAKER_SYNTAX_SUFIX = ']'
 export const FAKER_SYNTAX_MATCHER = /\[#faker:(.*?)\]/g
 
-export const FOR_LOOP_SYNTAX_MATCHER = /"#for (\w+) in (.*?)",(.*?),"#endfor"/g
+export const FOR_LOOP_SYNTAX_MATCHER = /"#for (.*?) in (.*?)",(.*?),"#endfor"/g
 export const FOR_LOOP_SYNTAX_PREFIX = '#for'
 export const FOR_LOOP_SYNTAX_SUFFIX = '#endfor'
