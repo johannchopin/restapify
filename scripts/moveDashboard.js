4dee7c8b494faabf6249e6fb88f24386addfba5a
johannchopin <johannchopin@pm.me>
2021-07-30 16:20:19 +0200

    🔨 First delete dashboard-public folder if exist

--- scripts/moveDashboard.js
+++ scripts/moveDashboard.js
@@ -1,9 +1,13 @@
 var path = require("path")
 var fs = require('fs-extra');
 
+const destPath = path.resolve(__dirname, '../src/dashboard-public')
+
+if (fs.existsSync(destPath)) fs.rmdirSync(destPath,  { recursive: true })
+
 fs.move(
   path.resolve(__dirname, '../node_modules/restapify-dashboard/public'),
-  path.resolve(__dirname, '../src/dashboard-public'),
+  destPath,
   err => {
     if (err) return console.error(err);
   }
